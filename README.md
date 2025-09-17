efc431d097695d8884e8e4187386958232cbee9b
Bruno Bernard <contact.brunobernard@gmail.com>
2025-09-17 23:40:13 +0400

    feat: first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# It's-a me Rasberry Pi
+
+I made a little script to tell which RPi is it, because I didn't label my pi, so i can run a script which blinks.
+
+My pattern was "It's-a me" from Mario.
+
---
+++ me.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+LED="/sys/class/leds/ACT"
+echo none | sudo tee $LED/trigger > /dev/null
+for i in {1..3}; do
+  echo 0 | sudo tee $LED/brightness > /dev/null; sleep 0.1
+  echo 1 | sudo tee $LED/brightness > /dev/null; sleep 0.1
+done
+sleep 0.5
+echo 0 | sudo tee $LED/brightness > /dev/null
+
